24a65b46adf16c9fe31fe9b21a884f7b7063b476
Upinder Singh <upinderawat@gmail.com>
2018-12-22 01:07:18 +0530

    Update settings.py

--- ABCwebsite/ABCwebsite/settings.py
+++ ABCwebsite/ABCwebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3vy&wlyziiaq+$0ft^(5cjpt&zka_1vb*g-vd)x9yfx*&4p#2p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
